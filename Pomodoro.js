f3b060f309e1e2fbc1442cc054ca8710a0c5c9e3
Ben Allen <ben.allen2@gmail.com>
2016-08-16 15:47:32 -0400

    mostly functional

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,6 +1,7 @@
 
 var sessionTimer = 25;
 var breakTimer = 5;
+var timeouts = [];
 
 function currentTime (){// sets the time at the bottom to current time based on browser location
 	var timeNow = new Date();
@@ -27,9 +28,13 @@ function timeCheck(i){// adds a zero infront of the time if it is less than 10
 }
 
 function startClick(){
-	$("#startBtn").click(function(){//changes the start to Stop, spins hourglass
-		$(this).html() == "Start!" ? $(this).html("Stop!") : $(this).html("Start!");
-		$("#hourglass").toggleClass( "fa-spin" );
+	$("#startBtn").click(function(){//spins hourglass
+		$("#hourglass").addClass( "fa-spin" );
+	});
+}
+function stopClick(){
+	$("#stopBtn").click(function(){
+		$("#hourglass").removeClass( "fa-spin" );
 	});
 }
 
@@ -58,12 +63,46 @@ $("#breakMinus").click(function(){
 	});
 };
 
-function countDown(duration, text){
+var b = function breakcountDown(duration, text){
 	var startTime = Date.now();
 	var difference;
 	var minutes;
 	var seconds;
+	text = document.getElementById("timer");
+	
+	function breakTimer(){
+		difference = duration - (((Date.now() - startTime) / 1000) | 0);
 
+		minutes = (difference / 60) | 0;
+		seconds = (difference % 60) | 0;
+
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        text.textContent = minutes + ":" + seconds;
+			if (minutes == "00" && seconds == "00"){
+					clearInterval(check);
+					$("#timer").empty();
+					a((parseInt($("#sessionTime").html()) * 60), text);
+					$("#command").html("Work Time!");
+				}
+			else if (difference <= 0) {
+            startTime = Date.now() + 1000;
+        }
+
+	};
+	transitionrev();
+	var check = setInterval(breakTimer, 1000);
+	timeouts.push(check);
+	
+}
+
+var a = function countDown(duration, text){
+	var startTime = Date.now();
+	var difference;
+	var minutes;
+	var seconds;
+	text = document.getElementById("timer");
 	
 	function sessionTimer(){
 		difference = duration - (((Date.now() - startTime) / 1000) | 0);
@@ -76,19 +115,34 @@ function countDown(duration, text){
 
         text.textContent = minutes + ":" + seconds;
 			if (minutes == "00" && seconds == "00"){
-					clearInterval(stopper);
+					clearInterval(check);
+					$("#timer").empty();
+					b((parseInt($("#breakTime").html()) * 60), text);
+					$("#command").html("Break Time!");
 				}
+			else if (difference <= 0) {
+           	 	startTime = Date.now() + 1000;
+        }
 	};
-
-	var stopper = setInterval(sessionTimer, 1000);
-
+	transitionfunc();
+	var check = setInterval(sessionTimer, 1000);
+	timeouts.push(check);
 }
 
 
 function transitionfunc(){// color change transition
-	var x = (parseInt($("#sessionTime").html()) * 60 );
+	var x = parseInt($("#sessionTime").html()) * 60;
 	document.getElementById("fillCircle").style.WebkitTransitionDuration = x + "s";
 	document.getElementById("fillCircle").style.transitionDuration = x + "s";
+	var here = $(".fillCircle");
+	here.addClass("red");
+}
+function transitionrev(){// color change transition
+	var x = parseInt($("#breakTime").html()) * 60;
+	document.getElementById("fillCircle").style.WebkitTransitionDuration = x + "s";
+	document.getElementById("fillCircle").style.transitionDuration = x + "s";
+	var here = $(".fillCircle");
+	here.removeClass("red");
 }
 
 $(document).ready(function(){
@@ -98,15 +152,22 @@ $(document).ready(function(){
 	plusIt();
 	minusIt();
 	startClick();
+	stopClick();
 
 	$("#startBtn").click(function(){
-		var breakDur = parseInt($("#breakTime").html()) * 60;
+		var breakDur = parseInt($("#breakTime").html()) * 60;// add *60 on finish
 		var duration = parseInt($("#sessionTime").html()) * 60;
 		text = document.querySelector("#timer");
-		countDown(duration, text);
-		var here = $(".fillCircle");
-		here.toggleClass("red");
-		transitionfunc();
+		a(duration, text);
+	});
+
+	$("#reset").click(function(){
+		location.reload();
+	});
+	$("#stopBtn").click(function(){
+		for(var i = 0; i < timeouts.length; i++){
+			clearInterval(timeouts[i]);
+		}
 	})
 
 });
--- timer.js
+++ timer.js
@@ -1,15 +1,43 @@
-function setDur(){
-
-}
-
-
-
-function countDown(duration, text, callback){
+var timeouts = [];
+var b = function breakcountDown(duration, text){
 	var startTime = Date.now();
 	var difference;
 	var minutes;
 	var seconds;
+	text = document.getElementById("countdown");
+	
+	function breakTimer(){
+		difference = duration - (((Date.now() - startTime) / 1000) | 0);
 
+		minutes = (difference / 60) | 0;
+		seconds = (difference % 60) | 0;
+
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        text.textContent = minutes + ":" + seconds;
+			if (minutes == "00" && seconds == "00"){
+					clearInterval(check);
+					$("#countdown").empty();
+					a(parseInt($("#timer").html()), text);
+				}
+			if (difference <= 0) {
+            startTime = Date.now() + 1000;
+        }
+	};
+
+	var check = setInterval(breakTimer, 1000);
+	timeouts.push(check);
+	
+}
+
+
+var a = function countDown(duration, text){
+	var startTime = Date.now();
+	var difference;
+	var minutes;
+	var seconds;
+	text = document.getElementById("countdown");
 	
 	function sessionTimer(){
 		difference = duration - (((Date.now() - startTime) / 1000) | 0);
@@ -23,20 +51,30 @@ function countDown(duration, text, callback){
         text.textContent = minutes + ":" + seconds;
 			if (minutes == "00" && seconds == "00"){
 					clearInterval(check);
+					$("#countdown").empty();
+					b(parseInt($("#breakTimer").html()), text);
 				}
+		if (difference <= 0) {
+            startTime = Date.now() + 1000;
+        }
 	};
 	
-
 	var check = setInterval(sessionTimer, 1000);
-	
+	timeouts.push( check );
 }
 
 
 $(document).ready(function(){
-	$(".btn").click(function(){
-		var breakDur = parseInt($("#breakTime").html()) * 60;
-		var duration = parseInt($("#timer").html()) * 60;
-		text = document.querySelector("#countdown");
-		countDown(duration, text);
-	})
-})
+	$("#start").click(function(){
+		var breakDur = parseInt($("#breakTimer").html());// need to put back *60 when done
+		var duration = parseInt($("#timer").html());// put back *60
+		text = document.getElementById("countdown");
+		a(duration, text);
+	
+		});	
+	$("#stop").click(function(){
+			for (var i = 0; i < timeouts.length; i++){
+				clearInterval(timeouts[i])
+			}
+		})
+	});
